624a36375bb2c040ce13664287089fc058d422d2
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-10-18 20:15:16 -0700

    Add changes to medium challenge

---
+++ codeChallenges/medium.js
@@ -0,0 +1,11 @@
+var goodDrivingRecord = true;
+var age = 24;
+
+if (goodDrivingRecord === true && age > 25) {
+    console.log(“Yah! You get a discount”);
+  } else if (goodDrivingRecord === true || age >25) {
+    console.log(“Sorry you have to pay full price”);
+  } else {DrivingRecord === false && age <25
+    console.log(“Sorry you need to have someone else sign for the rental”);
+ 
+}
