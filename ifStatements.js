71538986018443fb5b6791606304a272fe795056
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-10-18 22:38:38 -0700

    Add changes to if statements

--- ifStatements.js
+++ ifStatements.js
@@ -57,3 +57,13 @@ if (numQuarters === 0) {
 var cupsOfFlour = 1;
 var hasSauce = true;
 
+var goodDrivingRecord = true;
+var age = 23;
+
+if (goodDrivingRecord === true && age > 25) {
+  console.log = ("Yah! You get a discount");
+} else if (goodDrivingRecord === true || age > 25) {
+  console.log = ("Sorry. Full price for you");
+} else {goodDrivingRecord === false & age >25
+  console.log = ("Try again when you're over 25 friend");
+}
