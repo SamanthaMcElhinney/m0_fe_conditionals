3029d986f3bc2c5bc43559469ecc040475085cf8
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-10-20 22:26:59 -0700

    Add changes to code challenges.

--- codeChallenges/medium.js
+++ codeChallenges/medium.js
@@ -5,7 +5,9 @@ if (goodDrivingRecord === true && age > 25) {
   console.log("Yah! You get a discount!");
  } else if (goodDrivingRecord === false && age > 25) {
     console.log ("Sorry. Full price for you")
- } else (age < 25)
- console.log("You need someone to else to sign for you")   
+ } else {
+   console.log("You need someone to else to sign for you")   
+ }
+ 
   
 
